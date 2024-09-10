3802cac7a749d4bea65a0c63b7ed09f96cc55fd3
Mathieu Ledru <matyo91@gmail.com>
2024-09-11 00:42:47 +0200

    :heavy_plus_sign: Add SassBundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
 ];
