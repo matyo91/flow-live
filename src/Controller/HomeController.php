4ced3db9d98d93cdb7ca4fa43c7e0067678a9c34
Mathieu Ledru <mathieu.ledru@twenga.com>
2024-09-10 16:05:25 +0200

    :art: Add layout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Seo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,7 +15,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'seo' => new Seo('Flow Live'),
         ]);
     }
 }
--- src/Twig/Components/Layout.php
+++ src/Twig/Components/Layout.php
@@ -2,11 +2,20 @@
 
 namespace App\Twig\Components;
 
+use App\Enum\ThemeEnum;
+use App\Model\Seo;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 final class Layout
 {
     use DefaultActionTrait;
+
+    #[LiveProp]
+    public Seo $seo;
+
+    #[LiveProp]
+    public ThemeEnum $theme = ThemeEnum::LIGHT;
 }
